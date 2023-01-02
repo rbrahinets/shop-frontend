058ce99c4daee854ea07486a24de0a7b1208b250
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-02 12:55:14 +0200

    Create Angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
+import {AppComponent} from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent,]
+})
+export class AppModule {
+}
