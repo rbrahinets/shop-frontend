11468e55f77c747a222224be766507dc21ea6dcb
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-05 00:39:03 +0200

    Add event for button

--- src/app/button/button/button.component.ts
+++ src/app/button/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'shop-button',
@@ -7,10 +7,15 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() text: string;
+  @Output() btnClick = new EventEmitter();
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    this.btnClick.emit();
   }
 }
