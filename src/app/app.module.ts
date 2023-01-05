5724a57be4f31c02a7e7e75f0f6a439bfd7424ee
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-05 03:46:49 +0200

    Move components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import {RouterModule, Routes} from '@angular/router';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header/header.component';
-import {ButtonComponent} from './button/button/button.component';
-import {FooterComponent} from './footer/footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {ButtonComponent} from './button/button.component';
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
   declarations: [
