b75b3c5a8460ac4a5d6c7500d569d8c6025cfd49
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-06 12:56:41 +0200

    Add method for getting links to LinkService

--- src/app/links/shared/link.service.ts
+++ src/app/links/shared/link.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Link} from "./link.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinkService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/links';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getLinks(): Observable<Link[]> {
+    return this.http.get<Link[]>(this.apiUrl);
   }
 }
