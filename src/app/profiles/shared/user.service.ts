be8dfdf71dfa912b1c177ce2c5e933bba3124ce9
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-10 21:16:33 +0200

    Add getting profile from service

--- src/app/profiles/shared/user.service.ts
+++ src/app/profiles/shared/user.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Profile} from "./profile.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/users';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getUserForProfile(id: number): Observable<Profile> {
+    return this.http.get<Profile>(`${this.apiUrl}/${id}`);
   }
 }
