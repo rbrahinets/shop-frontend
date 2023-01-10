25b0751590f51cf4fff1c498aa4b4ba305f2e0a2
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-10 14:49:37 +0200

    Add getting categories in 'CategoryService'

--- src/app/categories/shared/category.service.ts
+++ src/app/categories/shared/category.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Category} from "./category.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/categories';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiUrl);
   }
 }
