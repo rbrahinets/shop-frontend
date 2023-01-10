c97b030dc3feec680f4e5c7a00e15729e48896fd
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-10 14:14:06 +0200

    Add getting products in 'ProductService'

--- src/app/products/shared/product.service.ts
+++ src/app/products/shared/product.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Product} from "./product.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/products';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
   }
 }
