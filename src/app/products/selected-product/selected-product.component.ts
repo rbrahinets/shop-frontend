c567f70949fda90007d232f1507f7bfc4f0540df
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-11 19:34:10 +0200

    Add display product by id from service

--- src/app/products/selected-product/selected-product.component.ts
+++ src/app/products/selected-product/selected-product.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Product} from '../shared/product.model';
+import {ProductService} from '../shared/product.service';
 
 @Component({
   selector: 'shop-selected-product',
@@ -6,9 +9,24 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./selected-product.component.css']
 })
 export class SelectedProductComponent implements OnInit {
-  constructor() {
+  product: Product;
+  imagePath: string;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router
+  ) {
   }
 
   ngOnInit(): void {
+    let path: string[] = (<string>this.router.url).split('/');
+    const id: number = +path[path.length - 1];
+
+    this.productService.getProduct(id).subscribe(
+      (product) => {
+        this.product = product;
+        this.imagePath = product.image;
+      }
+    );
   }
 }
