a246cdc4251ef26402a6a2abcabf6734a776ae55
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-12 01:58:57 +0200

    Refactor service for product

--- src/app/products/shared/product.service.ts
+++ src/app/products/shared/product.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Product} from "./product.model";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Product} from './product.model';
 
 @Injectable({
   providedIn: 'root'
