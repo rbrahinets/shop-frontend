7ce781e9c24c928e3ab075f3b7e538541815f1fc
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-11 20:02:30 +0200

    Create new service for products-category

---
+++ src/app/categories/shared/products-category.service.spec.ts
@@ -0,0 +1,16 @@
+// import {TestBed} from '@angular/core/testing';
+//
+// import {ProductsCategoryService} from './products-category.service';
+//
+// describe('ProductsCategoryService', () => {
+//   let service: ProductsCategoryService;
+//
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(ProductsCategoryService);
+//   });
+//
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
