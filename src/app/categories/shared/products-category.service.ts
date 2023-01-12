6fb700996cb35d050e616988875862231be23205
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-12 02:00:16 +0200

    Add getting products-categories in 'ProductsCategoryService'

--- src/app/categories/shared/products-category.service.ts
+++ src/app/categories/shared/products-category.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ProductsCategoryDto} from './products-category.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsCategoryService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/products-category';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getProductsCategory(): Observable<ProductsCategoryDto[]> {
+    return this.http.get<ProductsCategoryDto[]>(this.apiUrl);
   }
 }
