18d06c601d389d480b9c96998551fded65e0e750
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-12 15:11:35 +0200

    Add event for 'log-in'

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,4 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Router} from '@angular/router';
+import {LogInDto} from './shared/log-in.dto';
+import {LogInConfig} from './shared/log-in.config';
+import {UserService} from '../users/shared/user.service';
+import {HeaderComponent} from '../header/header.component';
 
 @Component({
   selector: 'shop-log-in',
@@ -6,9 +11,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-  constructor() {
+  @Output() onLogIn: EventEmitter<LogInDto> = new EventEmitter();
+  login: string;
+  password: string;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.userService.getUsers().subscribe(
+      (users) => {
+        for (const user of users) {
+          if (
+            (this.login === user.email || this.login === user.phone)
+            && this.password === user.password
+          ) {
+            LogInConfig.userId = user.id;
+            HeaderComponent._logged = !HeaderComponent._logged;
+            this.router.navigate(['/']).then(() => {});
+          }
+        }
+      }
+    );
   }
 }
