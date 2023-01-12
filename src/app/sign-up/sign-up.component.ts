c802f00a68332fdd7f0029aeecccee007f79234f
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-13 01:25:27 +0200

    Add sign up new user

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {UserService} from '../users/shared/user.service';
+import {User} from '../users/shared/user.model';
 
 @Component({
   selector: 'shop-sign-up',
@@ -6,9 +9,40 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  constructor() {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
+  confirmPassword: string;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.userService.getUsers().subscribe(
+      (users) => {
+        const id = (users.length as number) + 1;
+
+        const newUser: User = {
+          id: id,
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          phone: this.phone,
+          password: this.password
+        };
+
+        this.userService.saveUser(newUser).subscribe();
+      }
+    );
+
+    this.router.navigate(['/sign-in']).then();
   }
 }
