d9850a56967bbc823023686e515ee29f266d1f08
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-13 02:13:43 +0200

    Add getting user-role in 'UserRoleService'

--- src/app/users/shared/user-role.service.ts
+++ src/app/users/shared/user-role.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {UserRoleDto} from './UserRoleDto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRoleService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/user-role';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getRoleForUser(userId: number): Observable<UserRoleDto> {
+    return this.http.get<UserRoleDto>(`${this.apiUrl}/${userId}`);
   }
 }
