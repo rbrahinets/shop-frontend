d3dcd8a32eba1176e509291957f6c4cbbb04177e
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-13 02:14:57 +0200

    Add getting user-role in 'header'

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Cookie} from 'ng2-cookies/ng2-cookies';
+import {UserRoleService} from '../users/shared/user-role.service';
 
 @Component({
   selector: 'shop-header',
@@ -10,20 +11,33 @@ import {Cookie} from 'ng2-cookies/ng2-cookies';
 export class HeaderComponent implements OnInit {
   title: string = 'Shop';
   logged: boolean;
+  userRole: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private userRoleService: UserRoleService
   ) {
   }
 
   ngOnInit(): void {
     let userId: number = Number(Cookie.get('userId'));
     this.logged = userId > 0;
+
+    if (userId > 0) {
+      this.userRoleService.getRoleForUser(userId).subscribe(
+        (userRole) => {
+          this.userRole = userRole.roleId === 1 ? 'ROLE_ADMIN' : 'ROLE_USER';
+          Cookie.set('userRole', this.userRole);
+        }
+      );
+    }
   }
 
   onToggleLogOut(): void {
     Cookie.set('userId', '0');
+    Cookie.set('userRole', '');
     this.logged = !this.logged;
+    window.location.reload();
   }
 
   hasRoute(route: string): boolean {
