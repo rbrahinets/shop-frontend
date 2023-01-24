844c95a302b44bec9fd663048d9a4d4ea97696f3
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-25 01:36:03 +0200

    Add getting products from cart in 'ProductsCartsService'

--- src/app/cart/shared/products-carts.service.ts
+++ src/app/cart/shared/products-carts.service.ts
@@ -1,9 +1,37 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {Product} from '../../products/shared/product.model';
+import {ProductsCartsDto} from './products-carts.dto';
+import {ProductService} from '../../products/shared/product.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsCartsService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/products-carts';
+
+  constructor(
+    private http: HttpClient,
+    private productService: ProductService
+  ) {
+  }
+
+  getProductsFromCart(cartId: number): Observable<Product[]> {
+    let products: Product[] = [];
+
+    this.http.get<ProductsCartsDto[]>(this.apiUrl).subscribe(
+      (productsCarts) => {
+        for (const productCart of productsCarts) {
+          if (productCart.cartId === cartId) {
+            this.productService.getProduct(productCart.productId).subscribe(
+              (product) => products.push(product)
+            )
+          }
+        }
+      }
+    );
+
+    return of(products);
   }
 }
