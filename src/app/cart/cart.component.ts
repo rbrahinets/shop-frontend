32df8096a9cfd430b7dd922be13680471270fe1b
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-25 00:50:15 +0200

    Add display cart from service

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Cart} from './shared/cart.model';
+import {CartService} from './shared/cart.service';
 
 @Component({
   selector: 'shop-cart',
@@ -6,9 +8,17 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  constructor() {
+  cart = new Cart();
+
+  constructor(
+    private cartService: CartService
+  ) {
   }
 
   ngOnInit(): void {
+    const id: number = 1;
+    this.cartService.getCart(id).subscribe(
+      (cart) => this.cart = cart
+    );
   }
 }
