63142ec88525d776f343e677797e304da0467057
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-25 01:34:09 +0200

    Create new service for products-carts

---
+++ src/app/cart/shared/products-carts.service.spec.ts
@@ -0,0 +1,16 @@
+// import {TestBed} from '@angular/core/testing';
+//
+// import {ProductsCartsService} from './products-carts.service';
+//
+// describe('ProductsCartsService', () => {
+//   let service: ProductsCartsService;
+//
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(ProductsCartsService);
+//   });
+//
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
