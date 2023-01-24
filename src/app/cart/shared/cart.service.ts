90f2ee6c701041e43595a838bac543d1e4cea7a1
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-25 00:48:59 +0200

    Add display info of cart in 'CartService'

--- src/app/cart/shared/cart.service.ts
+++ src/app/cart/shared/cart.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Cart} from './cart.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/carts';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getCart(id: number): Observable<Cart> {
+    return this.http.get<Cart>(`${this.apiUrl}/${id}`);
   }
 }
