fffa088539b720f42553aee1e45b162206ed85b2
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-26 14:37:56 +0200

    Add getting wallets in 'WalletService'

--- src/app/wallet/shared/wallet.service.ts
+++ src/app/wallet/shared/wallet.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Wallet} from './wallet.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/wallets';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getWallets(): Observable<Wallet[]> {
+    return this.http.get<Wallet[]>(this.apiUrl);
   }
 }
