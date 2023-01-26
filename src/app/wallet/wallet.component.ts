9bd44fd67642ceaf774dd45286cc6e028e43c11b
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-26 14:48:42 +0200

    Add display wallet from service

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Wallet} from './shared/wallet.model';
+import {WalletService} from "./shared/wallet.service";
+import {Cookie} from 'ng2-cookies/ng2-cookies';
 
 @Component({
   selector: 'shop-wallet',
@@ -6,9 +9,32 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./wallet.component.css']
 })
 export class WalletComponent implements OnInit {
-  constructor() {
+  wallet = new Wallet();
+
+  constructor(
+    private walletService: WalletService
+  ) {
   }
 
   ngOnInit(): void {
+    const userId: number = Number(Cookie.get('userId'));
+
+    this.walletService.getWallets().subscribe(
+      (wallets) => {
+        let walletId: number = 0;
+
+        for (const wallet of wallets) {
+          if (wallet.userId === userId) {
+            walletId = wallet.id;
+          }
+        }
+
+        this.walletService.getWalletById(walletId).subscribe(
+          (wallet) => {
+            this.wallet = wallet;
+          }
+        );
+      }
+    );
   }
 }
