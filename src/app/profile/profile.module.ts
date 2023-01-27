e2feac7999153443c02f584991079adeffd99ddb
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-27 04:11:30 +0200

    Add 'CommonModule' to module for 'profile'

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {ProfileComponent} from './profile.component';
 
 @NgModule({
+  imports: [CommonModule],
   declarations: [ProfileComponent],
   exports: [ProfileComponent]
 })
