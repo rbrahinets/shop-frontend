e36891032053a78e59e89cf4c23b6b81d125db96
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-01-30 20:43:16 +0200

    Single out module for 'cart'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 import {ButtonModule} from './button/button.module';
 import {SignInModule} from './sign-in/sign-in.module';
@@ -20,14 +19,13 @@ import {MainModule} from './main/main.module';
 import {MainRoutingModule} from './main/main-routing.module';
 import {CategoriesModule} from './categories/categories.module';
 import {CategoriesRoutingModule} from './categories/categories-routing.module';
+import {CartModule} from './cart/cart.module';
 
 import {AppComponent} from './app.component';
 import {CartComponent} from './cart/cart.component';
 import {WalletComponent} from './wallet/wallet.component';
 
-import {CartService} from './cart/shared/cart.service';
 import {WalletService} from './wallet/shared/wallet.service';
-import {ProductsCartsService} from './cart/shared/products-carts.service';
 import {ProductsCategoryService} from './categories/shared/products-category.service';
 import {UserService} from './users/shared/user.service';
 import {UserRoleService} from './users/shared/user-role.service';
@@ -40,12 +38,10 @@ const shopRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent,
     WalletComponent,
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot(shopRoutes, {enableTracing: true}),
@@ -64,13 +60,12 @@ const shopRoutes: Routes = [
     MainRoutingModule,
     CategoriesModule,
     CategoriesRoutingModule,
+    CartModule,
   ],
   providers: [
     ProductsCategoryService,
     UserService,
     UserRoleService,
-    CartService,
-    ProductsCartsService,
     WalletService,
   ],
   bootstrap: [AppComponent]
---
+++ src/app/cart/cart.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {CartComponent} from './cart.component';
+import {CartService} from './shared/cart.service';
+import {ProductsCartsService} from './shared/products-carts.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+  ],
+  declarations: [CartComponent],
+  exports: [CartComponent],
+  providers: [
+    CartService,
+    ProductsCartsService
+  ]
+})
+export class CartModule {
+}
