3f3d5e0a68bf7a43ab15ef597eb443fe86838e68
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-02 01:23:31 +0200

    Add provider to module of 'header'

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -5,19 +5,21 @@ import {HttpClientModule} from '@angular/common/http';
 import {ButtonModule} from '../button/button.module';
 import {HeaderComponent} from './header.component';
 import {NavigationService} from '../shared/navigation.service';
+import {UserService} from '../users/shared/user.service';
 import {UserRoleService} from '../users/shared/user-role.service';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    ButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule
   ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
   providers: [
     NavigationService,
+    UserService,
     UserRoleService
   ]
 })
