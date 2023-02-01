4e9c420a5406c834857d238fa7021d8998b4c027
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-01 21:24:11 +0200

    Add method for navigation to endpoint in 'NavigationService'

--- src/app/shared/navigation.service.ts
+++ src/app/shared/navigation.service.ts
@@ -1,9 +1,18 @@
 import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
-  constructor() {
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  goToEndpoint(endpoint: string) {
+    if (endpoint) {
+      this.router.navigate([endpoint]).then();
+    }
   }
 }
