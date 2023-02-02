5a897422994c3d3f041aeedef13c330f38a8c28c
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-02 02:41:32 +0200

    Add validation of login

--- src/app/sign-in/sign-in.validator.ts
+++ src/app/sign-in/sign-in.validator.ts
@@ -1,2 +1,16 @@
+import {SignInDto} from './sign-in.dto';
+
 export class SignInValidator {
+  static validate(credential: SignInDto): boolean {
+    return SignInValidator.validateLogin(credential.login);
+  }
+
+  private static validateLogin(login: string): boolean {
+    if (!login) {
+      alert('You haven\'t entered a login');
+      return false;
+    }
+
+    return true;
+  }
 }
