e5ca9c44e448b834df249e37386ba4fb78cb1339
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-02 03:52:20 +0200

    Add validation of first name

--- src/app/sign-up/sign-up.validator.ts
+++ src/app/sign-up/sign-up.validator.ts
@@ -1,2 +1,16 @@
+import {SignUpDto} from './sign-up.dto';
+
 export class SignUpValidator {
+  static validate(credential: SignUpDto): boolean {
+    return SignUpValidator.validateFirstName(credential.firstName);
+  }
+
+  private static validateFirstName(firstName: string): boolean {
+    if (!firstName) {
+      alert('You haven\'t entered a first name');
+      return false;
+    }
+
+    return true;
+  }
 }
