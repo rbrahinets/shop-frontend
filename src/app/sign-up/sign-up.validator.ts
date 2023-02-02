3cc6aeecce48ca042c4b7cb383a04ac963e5e692
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-02 03:53:37 +0200

    Add validation of last name

--- src/app/sign-up/sign-up.validator.ts
+++ src/app/sign-up/sign-up.validator.ts
@@ -2,12 +2,22 @@ import {SignUpDto} from './sign-up.dto';
 
 export class SignUpValidator {
   static validate(credential: SignUpDto): boolean {
-    return SignUpValidator.validateFirstName(credential.firstName);
+    return SignUpValidator.validateFirstName(credential.firstName)
+      && SignUpValidator.validateLastName(credential.lastName);
   }
 
   private static validateFirstName(firstName: string): boolean {
     if (!firstName) {
       alert('You haven\'t entered a first name');
+      return false;
+    }
+
+    return true;
+  }
+
+  private static validateLastName(lastName: string): boolean {
+    if (!lastName) {
+      alert('You haven\'t entered a last name');
       return false;
     }
 
