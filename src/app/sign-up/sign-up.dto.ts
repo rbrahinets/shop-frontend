58caaf9af00e3d592b9aab5a44547d574fba1e6f
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-02 03:47:51 +0200

    Create dto for 'sign-up'

---
+++ src/app/sign-up/sign-up.dto.ts
@@ -0,0 +1,35 @@
+export class SignUpDto {
+  constructor(
+    private _firstName: string,
+    private _lastName: string,
+    private _email: string,
+    private _phone: string,
+    private _password: string,
+    private _confirmPassword: string
+  ) {
+  }
+
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get phone(): string {
+    return this._phone;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  get confirmPassword(): string {
+    return this._confirmPassword;
+  }
+}
