7d95d12b578e3c30367a3d17042d6fda7e77e678
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-02 02:41:59 +0200

    Add validation of password

--- src/app/sign-in/sign-in.validator.ts
+++ src/app/sign-in/sign-in.validator.ts
@@ -2,12 +2,22 @@ import {SignInDto} from './sign-in.dto';
 
 export class SignInValidator {
   static validate(credential: SignInDto): boolean {
-    return SignInValidator.validateLogin(credential.login);
+    return SignInValidator.validateLogin(credential.login)
+      && SignInValidator.validatePassword(credential.password);
   }
 
   private static validateLogin(login: string): boolean {
     if (!login) {
       alert('You haven\'t entered a login');
+      return false;
+    }
+
+    return true;
+  }
+
+  private static validatePassword(password: string): boolean {
+    if (!password) {
+      alert('You haven\'t entered a password');
       return false;
     }
 
