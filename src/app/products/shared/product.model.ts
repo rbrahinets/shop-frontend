94c98f503b4533394555496ee0fabb315b4b0c3d
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-04 03:15:11 +0200

    Refactor model for products

--- src/app/products/shared/product.model.ts
+++ src/app/products/shared/product.model.ts
@@ -1,9 +1,40 @@
 export class Product {
-  id?: number;
-  name: string;
-  describe: string;
-  price: number;
-  barcode: string;
-  inStock: boolean;
-  image: string;
+  constructor(
+    private _id?: number,
+    private _name?: string,
+    private _describe?: string,
+    private _price?: number,
+    private _barcode?: string,
+    private _inStock?: boolean,
+    private _image?: string
+  ) {
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get describe(): string {
+    return this._describe;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  get barcode(): string {
+    return this._barcode;
+  }
+
+  get inStock(): boolean {
+    return this._inStock;
+  }
+
+  get image(): string {
+    return this._image;
+  }
 }
