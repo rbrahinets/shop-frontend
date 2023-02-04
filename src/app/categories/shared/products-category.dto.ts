2f45d42cd347c01e3f9cc011960b63776f9842e3
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-04 03:11:45 +0200

    Refactor dto for products-category

--- src/app/categories/shared/products-category.dto.ts
+++ src/app/categories/shared/products-category.dto.ts
@@ -1,4 +1,15 @@
 export class ProductsCategoryDto {
-  productId: number;
-  categoryId: number;
+  constructor(
+    private _productId: number,
+    private _categoryId: number
+  ) {
+  }
+
+  get productId(): number {
+    return this._productId;
+  }
+
+  get categoryId(): number {
+    return this._categoryId;
+  }
 }
