fd9c45ab551e75f0f27ad762898646a5d9129af3
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-04 03:21:25 +0200

    Refactor model for wallet

--- src/app/wallet/shared/wallet.model.ts
+++ src/app/wallet/shared/wallet.model.ts
@@ -1,6 +1,37 @@
 export class Wallet {
-  id?: number;
-  number: string;
-  amountOfMoney: number;
-  userId: number;
+  constructor(
+    private _id?: number,
+    private _number?: string,
+    private _amountOfMoney?: number,
+    private _userId?: number
+  ) {
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  set number(value: string) {
+    this._number = value;
+  }
+
+  get amountOfMoney(): number {
+    return this._amountOfMoney;
+  }
+
+  set amountOfMoney(value: number) {
+    this._amountOfMoney = value;
+  }
+
+  get userId(): number {
+    return this._userId;
+  }
+
+  set userId(value: number) {
+    this._userId = value;
+  }
 }
