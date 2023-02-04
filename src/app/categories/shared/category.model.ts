577dbe75507991f0eb9d9ecd99d24ae856a4df39
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-04 03:09:20 +0200

    Refactor model for category

--- src/app/categories/shared/category.model.ts
+++ src/app/categories/shared/category.model.ts
@@ -1,4 +1,15 @@
 export class Category {
-  id?: number;
-  name: string;
+  constructor(
+    private _id?: number,
+    private _name?: string
+  ) {
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
 }
