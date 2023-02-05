9f5f63a87ddf2821b71bcc893b69aac4801a8cab
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-06 01:42:26 +0200

    Refactor 'SignUpComponent::signUp'

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -65,14 +65,13 @@ export class SignUpComponent implements OnInit {
   private signUp() {
     this.userService.getUsers().subscribe(
       (users) => {
-        const user = new User(
-          (users.length as number) + 1,
-          this.firstName,
-          this.lastName,
-          this.email,
-          this.phone,
-          this.password
-        );
+        const user = new User();
+        user.id = (users.length as number) + 1;
+        user.firstName = this.firstName;
+        user.lastName = this.lastName;
+        user.email = this.email;
+        user.phone = this.phone;
+        user.password = this.password;
 
         this.addNewUser(user);
         this.addRoleForNewUser(user);
