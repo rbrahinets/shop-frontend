84668c3e037cbaca9c9d08a68f16497efa581044
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-09 00:02:04 +0200

    Import modules for 'StripePaymentModule'

--- src/app/stripe-payment/stripe-payment.module.ts
+++ src/app/stripe-payment/stripe-payment.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {ButtonModule} from '../button/button.module';
 import {StripePaymentComponent} from './stripe-payment.component';
 import {StripePaymentService} from './shared/stripe-payment.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
+    ButtonModule
   ],
   declarations: [StripePaymentComponent],
   providers: [StripePaymentService],
