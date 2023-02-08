05b14c527595335b2779ade81f948f717ef9b213
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-09 00:07:01 +0200

    Add method for making payment to service

--- src/app/stripe-payment/shared/stripe-payment.service.ts
+++ src/app/stripe-payment/shared/stripe-payment.service.ts
@@ -1,9 +1,19 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StripePaymentService {
-  constructor() {
+  private apiUrl: string = 'http://localhost:8080/checkout/';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  makePayment(stripeToken: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, {token: stripeToken});
   }
 }
