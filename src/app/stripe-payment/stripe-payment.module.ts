6c8b406b2f14e54cf3a9b27a260831b2e3485e17
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-09 00:01:14 +0200

    Create new service for 'stripe-payment'

--- src/app/stripe-payment/stripe-payment.module.ts
+++ src/app/stripe-payment/stripe-payment.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {StripePaymentComponent} from './stripe-payment.component';
+import {StripePaymentService} from './shared/stripe-payment.service';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
   declarations: [StripePaymentComponent],
-  providers: [],
+  providers: [StripePaymentService],
   exports: [StripePaymentComponent]
 })
 export class StripePaymentModule {
