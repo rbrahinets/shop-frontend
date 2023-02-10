14a8ab1cc6b015fa9185685f1468201a69ccf588
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-10 13:49:42 +0200

    Add method for validation phone uniqueness

--- src/app/shared/validators/phone.validator.ts
+++ src/app/shared/validators/phone.validator.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {User} from '../../users/shared/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,22 @@ export class PhoneValidator {
   private static isInvalidPhone(phone: string): boolean {
     return !phone.startsWith('+')
       || phone.length < 12;
+  }
+
+  private static isUniquePhone(
+    phone: string,
+    users: User[]
+  ) {
+    if (!users) {
+      return true;
+    }
+
+    for (const user of users) {
+      if (user.phone === phone) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
