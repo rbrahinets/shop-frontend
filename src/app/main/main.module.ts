4243d9611a78aefc6adcc17f52491a57a06fce30
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 19:45:59 +0200

    Import module for 'MainModule'

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {ProductsModule} from '../products/products.module';
+import {SortModule} from '../sort/sort.module';
 import {MainComponent} from './main.component';
 import {ProductService} from '../products/shared/product.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     ProductsModule,
-    HttpClientModule
+    SortModule
   ],
   declarations: [MainComponent],
   providers: [ProductService],
