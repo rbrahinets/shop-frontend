397b231eeed1f689bc023d93d8efbc083384becb
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 16:52:32 +0200

    Add method for event on click 'Edit'

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {User} from '../users/shared/user.model';
 import {UserService} from '../users/shared/user.service';
+import {NavigationService} from '../shared/navigation.service';
 import {LoggedUserService} from '../users/shared/logged-user.service';
 
 @Component({
@@ -12,12 +14,19 @@ export class ProfileComponent implements OnInit {
   user = new User();
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private router: Router,
+    private navigation: NavigationService
   ) {
+    this.navigation = new NavigationService(this.router);
   }
 
   ngOnInit(): void {
     this.setProfileOfUser();
+  }
+
+  onClickEdit(): void {
+    this.navigation.goToEndpoint('/profile/edit');
   }
 
   private setProfileOfUser(): void {
