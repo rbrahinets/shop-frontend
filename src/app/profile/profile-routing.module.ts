eaa0df4ea9095c92b76d89e493795edae08cdb83
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 16:50:35 +0200

    Add new rout for routing of 'profile'

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProfileComponent} from './profile.component';
+import {ProfileEditComponent} from './profile-edit/profile-edit.component';
 
 const profileRoutes: Routes = [
   {path: 'profile', component: ProfileComponent},
+  {path: 'profile/edit', component: ProfileEditComponent},
 ];
 
 @NgModule({
