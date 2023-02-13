740fb2d075bc3f662d2f372f3e055f4e57fb17ec
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 20:24:11 +0200

    Add method for sorting categories

--- src/app/sort/sort-categories/sort-categories.component.ts
+++ src/app/sort/sort-categories/sort-categories.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Category} from '../../categories/shared/category.model';
+import {SortCategoriesService} from '../shared/sort-categories.service';
 
 @Component({
   selector: 'shop-categories-sort',
@@ -6,9 +8,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sort-categories.component.css']
 })
 export class SortCategoriesComponent implements OnInit {
-  constructor() {
+  @Input() categories: Category[];
+  sortTypes: string[];
+
+  constructor(
+    private sortCategoriesService: SortCategoriesService
+  ) {
+    this.sortTypes = ['Name[A-Z]', 'Name[Z-A]'];
   }
 
   ngOnInit(): void {
+  }
+
+  sortCategories(typeSort: string): void {
+    if (typeSort === this.sortTypes[0]) {
+      this.categories = this.sortCategoriesService.sortByNameAsc(this.categories);
+    } else if (typeSort === this.sortTypes[1]) {
+      this.categories = this.sortCategoriesService.sortByNameDesc(this.categories);
+    }
   }
 }
