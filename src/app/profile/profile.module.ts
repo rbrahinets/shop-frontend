56c45dd7e201ebbfc5bff8b48365d0604782e03b
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 16:47:35 +0200

    Add module for 'profile'

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
+import {ButtonModule} from '../button/button.module';
 import {ProfileComponent} from './profile.component';
 import {UserService} from '../users/shared/user.service';
 import {LoggedUserService} from '../users/shared/logged-user.service';
@@ -8,7 +9,8 @@ import {LoggedUserService} from '../users/shared/logged-user.service';
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule
   ],
   declarations: [ProfileComponent],
   providers: [
