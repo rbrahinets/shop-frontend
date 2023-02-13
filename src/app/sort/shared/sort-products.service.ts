6835d5ef66cc852a5e4b24b0d7ae46186395a725
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 19:40:04 +0200

    Add method for sorting products by name asc to service

--- src/app/sort/shared/sort-products.service.ts
+++ src/app/sort/shared/sort-products.service.ts
@@ -1,7 +1,24 @@
 import {Injectable} from '@angular/core';
+import {Product} from '../../products/shared/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SortProductsService {
+  sortByNameAsc(products: Product[]): Product[] {
+    return products.sort(
+      (
+        product1: Product,
+        product2: Product
+      ) => {
+        if (product1.name > product2.name) {
+          return 1;
+        } else if (product1.name < product2.name) {
+          return -1;
+        }
+
+        return 0;
+      }
+    );
+  }
 }
