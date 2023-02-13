161503f6cfebb57fcf6336aca880db6c8110dae3
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 19:35:06 +0200

    Add provider to module for 'sort-products'

--- src/app/sort/sort.module.ts
+++ src/app/sort/sort.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SortProductsComponent} from './sort-products/sort-products.component';
+import {SortProductsService} from './shared/sort-products.service';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [SortProductsComponent],
-  providers: [],
+  providers: [SortProductsService],
   exports: [SortProductsComponent]
 })
 export class SortModule {
