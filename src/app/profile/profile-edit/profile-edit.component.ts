8ec0087b6547586696bc6bc2bfe6a96e45957d0b
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 16:45:09 +0200

    Create new component 'profile-edit'

---
+++ src/app/profile/profile-edit/profile-edit.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'shop-profile-edit',
+  templateUrl: './profile-edit.component.html',
+  styleUrls: ['./profile-edit.component.css']
+})
+export class ProfileEditComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
