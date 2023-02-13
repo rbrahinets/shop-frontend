c7abd4143c8874f7ab47c1095cb12441d226752a
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 20:22:43 +0200

    Add method for sorting categories by name asc to service

--- src/app/sort/shared/sort-categories.service.ts
+++ src/app/sort/shared/sort-categories.service.ts
@@ -1,7 +1,24 @@
 import {Injectable} from '@angular/core';
+import {Category} from '../../categories/shared/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SortCategoriesService {
+  sortByNameAsc(categories: Category[]): Category[] {
+    return categories.sort(
+      (
+        category1: Category,
+        category2: Category
+      ) => {
+        if (category1.name > category2.name) {
+          return 1;
+        } else if (category1.name < category2.name) {
+          return -1;
+        }
+
+        return 0;
+      }
+    );
+  }
 }
