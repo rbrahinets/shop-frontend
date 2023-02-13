d283551a18c97801b8d5b06b0c323497fd13deb3
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-13 16:49:38 +0200

    Add component 'profile-edit' to module for 'profile'

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {ButtonModule} from '../button/button.module';
 import {ProfileComponent} from './profile.component';
+import {ProfileEditComponent} from './profile-edit/profile-edit.component';
 import {UserService} from '../users/shared/user.service';
 import {NavigationService} from '../shared/navigation.service';
 import {LoggedUserService} from '../users/shared/logged-user.service';
@@ -13,13 +14,19 @@ import {LoggedUserService} from '../users/shared/logged-user.service';
     HttpClientModule,
     ButtonModule
   ],
-  declarations: [ProfileComponent],
+  declarations: [
+    ProfileComponent,
+    ProfileEditComponent
+  ],
   providers: [
     UserService,
     NavigationService,
     LoggedUserService
   ],
-  exports: [ProfileComponent]
+  exports: [
+    ProfileComponent,
+    ProfileEditComponent
+  ]
 })
 export class ProfileModule {
 }
