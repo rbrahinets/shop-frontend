de731cbd8f2b35f8d7cdfb7ecceec09421fcb18e
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-15 15:36:19 +0200

    Add method for deleting category to service

--- src/app/categories/shared/category.service.ts
+++ src/app/categories/shared/category.service.ts
@@ -26,4 +26,10 @@ export class CategoryService {
       Http.getHttpOptions()
     );
   }
+
+  deleteCategory(category: Category): void {
+    this.http.delete<Category>(
+      `${this.apiUrl}/${category.id}`
+    ).subscribe();
+  }
 }
