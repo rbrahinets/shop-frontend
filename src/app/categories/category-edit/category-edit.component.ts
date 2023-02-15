9d7696e1f0a8f5ed99863e8f71dae182c40b6edd
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-15 17:04:55 +0200

    Add method for setting current category

--- src/app/categories/category-edit/category-edit.component.ts
+++ src/app/categories/category-edit/category-edit.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {CategoryService} from '../shared/category.service';
+import {NavigationService} from '../../shared/navigation.service';
+import {Category} from '../shared/category.model';
 
 @Component({
   selector: 'shop-category-edit',
@@ -6,9 +10,30 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./category-edit.component.css']
 })
 export class CategoryEditComponent implements OnInit {
-  constructor() {
+  category: Category;
+
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private navigation: NavigationService
+  ) {
+    this.navigation = new NavigationService(this.router);
+    this.category = new Category();
   }
 
   ngOnInit(): void {
+    this.setCurrentCategory();
+  }
+
+  private setCurrentCategory() {
+    this.categoryService.getCategories().subscribe(
+      (categories: Category[]) => {
+        for (const category of categories) {
+          if (category.id === this.navigation.getCurrentPathId()) {
+            this.category = category;
+          }
+        }
+      }
+    );
   }
 }
