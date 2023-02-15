1e7ffafaad515c78f5cad569b3c21c37d7f86539
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-15 14:44:18 +0200

    Add method for saving category to service

--- src/app/categories/shared/category.service.ts
+++ src/app/categories/shared/category.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Category} from './category.model';
+import {Http} from '../../shared/http';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,13 @@ export class CategoryService {
 
   getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(this.apiUrl);
+  }
+
+  saveCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(
+      this.apiUrl,
+      category,
+      Http.getHttpOptions()
+    );
   }
 }
