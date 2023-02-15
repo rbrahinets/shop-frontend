84271e7f2ead6b96bc52c7eac024ad171f355f7e
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-15 15:05:34 +0200

    Add routing for page 'category-delete'

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {CategoryListComponent} from './category-list/category-list.component';
 import {CategoryComponent} from './category/category.component';
 import {CategoryAddComponent} from './category-add/category-add.component';
+import {CategoryDeleteComponent} from './category-delete/category-delete.component';
 import {RoleAdminGuard} from '../shared/role-admin-guard';
 
 const categoriesRoutes: Routes = [
   {path: 'categories', component: CategoryListComponent},
   {path: 'categories/add', component: CategoryAddComponent, canActivate: [RoleAdminGuard]},
+  {path: 'categories/delete', component: CategoryDeleteComponent, canActivate: [RoleAdminGuard]},
   {path: 'categories/:id', component: CategoryComponent},
 ];
 
