b5aa04dcf2ea09da29fa6a0e4435c4c34b917e65
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-15 14:51:12 +0200

    Add method for adding category

--- src/app/categories/category-add/category-add.component.ts
+++ src/app/categories/category-add/category-add.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {CategoryService} from '../shared/category.service';
+import {Category} from '../shared/category.model';
 
 @Component({
   selector: 'shop-category-add',
@@ -8,9 +11,23 @@ import {Component, OnInit} from '@angular/core';
 export class CategoryAddComponent implements OnInit {
   categoryName: string;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  private addCategory(): void {
+    this.categoryService.getCategories().subscribe(
+      (categories: Category[]) => {
+        const newCategory = new Category();
+        newCategory.id = categories.length + 1;
+        newCategory.name = this.categoryName;
+        this.categoryService.saveCategory(newCategory).subscribe();
+      }
+    );
   }
 }
