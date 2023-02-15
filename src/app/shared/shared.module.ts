bfa3a93915d79e4cd02835e07c7485c410c515b2
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-15 14:14:17 +0200

    Add provider to module for 'shared'

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
+import {LoggedUserService} from '../users/shared/logged-user.service';
 import {NavigationService} from './navigation.service';
 
 @NgModule({
   providers: [
-    NavigationService,
+    LoggedUserService,
+    NavigationService
   ],
 })
 export class SharedModule {
