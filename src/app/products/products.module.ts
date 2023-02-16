f677af92d6242831e7c64d40d96e95333ada90bf
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 16:29:13 +0200

    Import module for module of 'products'

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 import {ButtonModule} from '../button/button.module';
 import {HttpClientModule} from '@angular/common/http';
 import {SortModule} from '../sort/sort.module';
@@ -18,6 +19,7 @@ import {NavigationService} from "../shared/navigation.service";
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
     ButtonModule,
     HttpClientModule,
     SortModule
