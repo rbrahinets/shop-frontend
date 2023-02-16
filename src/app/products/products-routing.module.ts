fb49ec74e6cd4ca11281d99b2014a7dfbfa8daec
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 21:36:00 +0200

    Add routing for page 'product-delete'

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductComponent} from './product/product.component';
 import {ProductAddComponent} from './product-add/product-add.component';
+import {ProductDeleteComponent} from './product-delete/product-delete.component';
 import {RoleAdminGuard} from '../shared/role-admin-guard';
 
 const productsRoutes: Routes = [
   {path: 'products', component: ProductListComponent},
   {path: 'products/add', component: ProductAddComponent, canActivate: [RoleAdminGuard]},
+  {path: 'products/delete', component: ProductDeleteComponent, canActivate: [RoleAdminGuard]},
   {path: 'products/:id', component: ProductComponent},
 ];
 
