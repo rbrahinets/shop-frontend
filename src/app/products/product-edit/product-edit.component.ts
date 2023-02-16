6192940a91018d640c49adee31a830c5b489bde5
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 23:46:35 +0200

    Add method for setting current product

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Product} from '../shared/product.model';
+import {NavigationService} from '../../shared/navigation.service';
 
 @Component({
   selector: 'shop-category-edit',
@@ -6,9 +9,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
-  constructor() {
+  product: Product;
+  productName: string;
+  productDescribe: string;
+  productPrice: number;
+  productInStock: string;
+  productCategory: string;
+
+  constructor(
+    private router: Router,
+    private navigation: NavigationService
+  ) {
+    this.product = new Product();
+    this.navigation = new NavigationService(router);
   }
 
   ngOnInit(): void {
+  }
+
+  private setCurrentProduct(products: Product[]): void {
+    for (const product of products) {
+      if (product.id === this.navigation.getCurrentPathId()) {
+        this.product = product;
+        this.productName = product.name;
+        this.productDescribe = product.describe;
+        this.productPrice = product.price;
+        this.productInStock = product.inStock ? 'Yes' : 'No';
+      }
+    }
   }
 }
