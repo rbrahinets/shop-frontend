ce09e8cd3fdc2e62f9a52b79c367e33ce68a1a0a
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 14:08:28 +0200

    Create validator for describe of product

---
+++ src/app/shared/validators/product-describe.validator.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDescribeValidator {
+  validate(productDescribe: string): boolean {
+    return ProductDescribeValidator.validateProductDescribe(
+      productDescribe
+    );
+  }
+
+  private static validateProductDescribe(productDescribe: string): boolean {
+    if (!productDescribe) {
+      alert('You haven\'t entered a describe of product');
+      return false;
+    } else if (ProductDescribeValidator.isInvalidProductDescribe(productDescribe)) {
+      alert('You have entered a short describe of product');
+      return false;
+    }
+
+    return true;
+  }
+
+  private static isInvalidProductDescribe(productDescribe: string): boolean {
+    return productDescribe.length < 10;
+  }
+}
