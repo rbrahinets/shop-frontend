b1b2fba1f841b4f90b47b23e627154ea09c81a9d
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 16:33:28 +0200

    Add routing for page 'product-add'

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,9 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductComponent} from './product/product.component';
+import {ProductAddComponent} from './product-add/product-add.component';
+import {RoleAdminGuard} from '../shared/role-admin-guard';
 
 const productsRoutes: Routes = [
   {path: 'products', component: ProductListComponent},
+  {path: 'products/add', component: ProductAddComponent, canActivate: [RoleAdminGuard]},
   {path: 'products/:id', component: ProductComponent},
 ];
 
