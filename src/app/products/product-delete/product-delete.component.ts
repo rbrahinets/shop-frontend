250c1608992cd84a7acd4c3467806694d2641ae2
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 22:10:45 +0200

    Add method for setting products

--- src/app/products/product-delete/product-delete.component.ts
+++ src/app/products/product-delete/product-delete.component.ts
@@ -1,14 +1,26 @@
 import {Component, OnInit} from '@angular/core';
-
+import {ProductService} from '../shared/product.service';
+import {Product} from '../shared/product.model';
 @Component({
   selector: 'shop-product-delete',
   templateUrl: './product-delete.component.html',
   styleUrls: ['./product-delete.component.css']
 })
 export class ProductDeleteComponent implements OnInit {
-  constructor() {
+  private products: Product[];
+
+  constructor(
+    private productService: ProductService
+  ) {
   }
 
   ngOnInit(): void {
+    this.setProducts();
+  }
+
+  private setProducts(): void {
+    this.productService.getProducts().subscribe(
+      (products: Product[]) => this.products = products
+    );
   }
 }
