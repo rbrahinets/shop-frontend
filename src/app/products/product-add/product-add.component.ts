3a321fea517da75a4e5ff3e05907bb73c8028376
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 16:59:20 +0200

    Add method for setting products

--- src/app/products/product-add/product-add.component.ts
+++ src/app/products/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ProductService} from '../shared/product.service';
 import {CategoryService} from '../../categories/shared/category.service';
+import {Product} from '../shared/product.model';
 import {Category} from '../../categories/shared/category.model';
 
 @Component({
@@ -9,19 +11,28 @@ import {Category} from '../../categories/shared/category.model';
 })
 export class ProductAddComponent implements OnInit {
   categories: Category[];
+  private products: Product[];
 
   constructor(
+    private productService: ProductService,
     private categoryService: CategoryService
   ) {
   }
 
   ngOnInit(): void {
     this.setCategories();
+    this.setProducts();
   }
 
   private setCategories(): void {
     this.categoryService.getCategories().subscribe(
       (categories: Category[]) => this.categories = categories
+    );
+  }
+
+  private setProducts(): void {
+    this.productService.getProducts().subscribe(
+      (products: Product[]) => this.products = products
     );
   }
 }
