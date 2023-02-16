749e9fe9076ddc206f2be2b095fd9d258d1fe416
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 16:38:00 +0200

    Add method for event on click 'Add New Product'

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {Product} from '../shared/product.model';
 import {ProductService} from '../shared/product.service';
+import {NavigationService} from '../../shared/navigation.service';
 
 @Component({
   selector: 'shop-product-list-item-list',
@@ -11,12 +13,19 @@ export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
   constructor(
-    private productService: ProductService
+    private productService: ProductService,
+    private router: Router,
+    private navigation: NavigationService
   ) {
+    this.navigation = new NavigationService(this.router);
   }
 
   ngOnInit(): void {
     this.setProducts();
+  }
+
+  onAddNewProduct(): void {
+    this.navigation.goToEndpoint('/products/add');
   }
 
   private setProducts() {
