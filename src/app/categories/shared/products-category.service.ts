9a815336512ed30260acae9e9ec2ce3fe5284590
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 16:42:47 +0200

    Add method for saving product to category

--- src/app/categories/shared/products-category.service.ts
+++ src/app/categories/shared/products-category.service.ts
@@ -2,6 +2,9 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ProductsCategoryDto} from './products-category.dto';
+import {Product} from '../../products/shared/product.model';
+import {Category} from './category.model';
+import {Http} from '../../shared/http';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +19,20 @@ export class ProductsCategoryService {
 
   getProductsCategory(): Observable<ProductsCategoryDto[]> {
     return this.http.get<ProductsCategoryDto[]>(this.apiUrl);
+  }
+
+  saveProductToCategory(
+    product: Product,
+    category: Category
+  ): Observable<ProductsCategoryDto> {
+    const productsCategory = new ProductsCategoryDto();
+    productsCategory.productId = product.id;
+    productsCategory.categoryId = category.id;
+
+    return this.http.post<ProductsCategoryDto>(
+      this.apiUrl,
+      productsCategory,
+      Http.getHttpOptions()
+    );
   }
 }
