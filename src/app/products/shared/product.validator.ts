70cf08e651b852b0a37288efa11236ac56a61a93
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 14:09:11 +0200

    Create validator for 'product'

---
+++ src/app/products/shared/product.validator.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {ProductNameValidator} from '../../shared/validators/product-name.validator';
+import {ProductDescribeValidator} from '../../shared/validators/product-describe.validator';
+import {ProductPriceValidator} from '../../shared/validators/product-price.validator';
+import {ProductBarcodeValidator} from '../../shared/validators/product-barcode.validator';
+import {ProductDto} from './product.dto';
+import {Product} from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductValidator {
+  constructor(
+    private productNameValidator: ProductNameValidator,
+    private productDescribeValidator: ProductDescribeValidator,
+    private productPriceValidator: ProductPriceValidator,
+    private productBarcodeValidator: ProductBarcodeValidator
+  ) {
+  }
+
+  validate(
+    product: ProductDto,
+    products: Product[],
+    isDelete: boolean = false
+  ): boolean {
+    return this.productNameValidator.validate(product.name)
+      && this.productDescribeValidator.validate(product.describe)
+      && this.productPriceValidator.validate(product.price)
+      && this.productBarcodeValidator.validate(
+        product.barcode,
+        products,
+        isDelete
+      );
+  }
+}
