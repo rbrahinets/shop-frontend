921bd32ed3db4ee4cf94f3a7b8f9d401023eb6b8
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 14:08:49 +0200

    Create validator for barcode of product

---
+++ src/app/shared/validators/product-barcode.validator.ts
@@ -0,0 +1,74 @@
+import {Injectable} from '@angular/core';
+import {Product} from '../../products/shared/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductBarcodeValidator {
+  validate(
+    productBarcode: string,
+    products: Product[],
+    isDelete: boolean = false
+  ): boolean {
+    return ProductBarcodeValidator.validateProductBarcode(
+      productBarcode,
+      products,
+      isDelete
+    );
+  }
+
+  private static validateProductBarcode(
+    productBarcode: string,
+    products: Product[],
+    isDelete: boolean = false
+  ): boolean {
+    if (!productBarcode) {
+      alert('You haven\'t entered a barcode of product');
+      return false;
+    } else if (ProductBarcodeValidator.isInvalidProductBarcode(productBarcode)) {
+      alert('You have entered a short barcode of product');
+      return false;
+    } else if (
+      ProductBarcodeValidator.isExistingProductBarcode(
+        productBarcode,
+        products
+      )
+      && !isDelete
+    ) {
+      alert('You have entered an existing barcode of product');
+      return false;
+    } else if (
+      !ProductBarcodeValidator.isExistingProductBarcode(
+        productBarcode,
+        products
+      )
+      && isDelete
+    ) {
+      alert('You have entered a non-existent barcode of product');
+      return false;
+    }
+
+    return true;
+  }
+
+  private static isInvalidProductBarcode(productBarcode: string): boolean {
+    return productBarcode.length < 2;
+  }
+
+  private static isExistingProductBarcode(
+    productBarcode: string,
+    products: Product[]
+  ): boolean {
+    if (!products) {
+      return false;
+    }
+
+    for (const product of products) {
+      if (product.barcode === productBarcode) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
