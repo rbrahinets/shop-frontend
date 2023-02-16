04aca3227f4da41880e82826de4cb4c504f25a56
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-16 22:23:04 +0200

    Add routing for page 'product-edit'

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductComponent} from './product/product.component';
 import {ProductAddComponent} from './product-add/product-add.component';
+import {ProductEditComponent} from './product-edit/product-edit.component';
 import {ProductDeleteComponent} from './product-delete/product-delete.component';
 import {RoleAdminGuard} from '../shared/role-admin-guard';
 
@@ -11,6 +12,7 @@ const productsRoutes: Routes = [
   {path: 'products/add', component: ProductAddComponent, canActivate: [RoleAdminGuard]},
   {path: 'products/delete', component: ProductDeleteComponent, canActivate: [RoleAdminGuard]},
   {path: 'products/:id', component: ProductComponent},
+  {path: 'products/:id/edit', component: ProductEditComponent, canActivate: [RoleAdminGuard]},
 ];
 
 @NgModule({
