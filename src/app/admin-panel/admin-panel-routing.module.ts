7032d876760c65e494933dfbdf5d7a8c62778b47
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-20 18:51:52 +0200

    Add routing for page 'user-delete'

--- src/app/admin-panel/admin-panel-routing.module.ts
+++ src/app/admin-panel/admin-panel-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminPanelComponent} from './admin-panel.component';
+import {UserDeleteComponent} from './user-delete/user-delete.component';
 import {RoleSuperadminGuard} from '../shared/role-superadmin.guard';
 
 const adminPanelRoutes: Routes = [
   {path: 'admin-panel', component: AdminPanelComponent, canActivate: [RoleSuperadminGuard]},
+  {path: 'admin-panel/delete-user', component: UserDeleteComponent, canActivate: [RoleSuperadminGuard]},
 ];
 
 @NgModule({
