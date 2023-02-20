38baaf6c99ff0bcbc7afe4fa3862c82ca2d9c855
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-02-20 18:55:15 +0200

    Add routing for page 'admin-number-add'

--- src/app/admin-panel/admin-panel-routing.module.ts
+++ src/app/admin-panel/admin-panel-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminPanelComponent} from './admin-panel.component';
 import {UserDeleteComponent} from './user-delete/user-delete.component';
+import {AdminNumberAddComponent} from './admin-number-add/admin-number-add.component';
 import {RoleSuperadminGuard} from '../shared/role-superadmin.guard';
 
 const adminPanelRoutes: Routes = [
   {path: 'admin-panel', component: AdminPanelComponent, canActivate: [RoleSuperadminGuard]},
   {path: 'admin-panel/delete-user', component: UserDeleteComponent, canActivate: [RoleSuperadminGuard]},
+  {path: 'admin-panel/add-admin-number', component: AdminNumberAddComponent, canActivate: [RoleSuperadminGuard]},
 ];
 
 @NgModule({
