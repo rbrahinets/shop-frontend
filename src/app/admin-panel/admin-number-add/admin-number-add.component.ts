a757407419e9193b608becccf61bbeb39a332c6a
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-01 20:36:41 +0200

    Add method for adding number of admin

--- src/app/admin-panel/admin-number-add/admin-number-add.component.ts
+++ src/app/admin-panel/admin-number-add/admin-number-add.component.ts
@@ -1,17 +1,24 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {AdminService} from '../../users/shared/admin.service';
 import {AdminNumberDto} from '../../users/shared/admin-number.dto';
+import {NavigationService} from '../../shared/navigation.service';
+
 @Component({
   selector: 'shop-admin-number-add',
   templateUrl: './admin-number-add.component.html',
   styleUrls: ['./admin-number-add.component.css']
 })
 export class AdminNumberAddComponent implements OnInit {
+  adminNumber: string;
   private adminNumbers: AdminNumberDto[];
 
   constructor(
+    private router: Router,
     private adminService: AdminService,
+    private navigation: NavigationService,
   ) {
+    this.navigation = new NavigationService(this.router);
   }
 
   ngOnInit(): void {
@@ -22,5 +29,13 @@ export class AdminNumberAddComponent implements OnInit {
     this.adminService.getAdminsNumbers().subscribe(
       (adminNumbers: AdminNumberDto[]) => this.adminNumbers = adminNumbers
     );
+  }
+
+  private addAdminNumber(): void {
+    const adminNumbersDto = new AdminNumberDto();
+    adminNumbersDto.id = this.adminNumbers.length + 1;
+    adminNumbersDto.number = this.adminNumber;
+    this.adminService.saveAdminNumber(adminNumbersDto).subscribe();
+    this.navigation.goToEndpoint('/admin-panel', true);
   }
 }
