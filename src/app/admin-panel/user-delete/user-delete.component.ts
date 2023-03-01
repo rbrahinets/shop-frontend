c331edbef4a42b85c162ebbb1f206dd846b45b11
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-01 21:30:18 +0200

    Add method for setting users

--- src/app/admin-panel/user-delete/user-delete.component.ts
+++ src/app/admin-panel/user-delete/user-delete.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../users/shared/user.service';
+import {User} from '../../users/shared/user.model';
 
 @Component({
   selector: 'shop-user-delete',
@@ -6,9 +8,20 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-delete.component.css']
 })
 export class UserDeleteComponent implements OnInit {
-  constructor() {
+  email: string;
+  private users: User[];
+
+  constructor(
+    private userService: UserService,
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  private setUsers(): void {
+    this.userService.getUsers().subscribe(
+      (users: User[]) => this.users = users
+    );
   }
 }
