19a6f57ab24b29ba83746173734b76f77693314d
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-01 20:59:07 +0200

    Add method for setting numbers of admins

--- src/app/admin-panel/admin-number-delete/admin-number-delete.component.ts
+++ src/app/admin-panel/admin-number-delete/admin-number-delete.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {AdminService} from '../../users/shared/admin.service';
+import {AdminNumberDto} from '../../users/shared/admin-number.dto';
 
 @Component({
   selector: 'shop-admin-number-delete',
@@ -6,9 +8,19 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./admin-number-delete.component.css']
 })
 export class AdminNumberDeleteComponent implements OnInit {
-  constructor() {
+  private adminsNumbers: AdminNumberDto[];
+
+  constructor(
+    private adminService: AdminService,
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  private setAdminNumbers(): void {
+    this.adminService.getAdminsNumbers().subscribe(
+      (adminNumbers: AdminNumberDto[]) => this.adminsNumbers = adminNumbers
+    );
   }
 }
