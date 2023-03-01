a35620d72c844070770ec2adae0466f8129fcda3
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-02 00:07:41 +0200

    Add method for sorting users by first name asc to service

--- src/app/sort/shared/sort-users.service.ts
+++ src/app/sort/shared/sort-users.service.ts
@@ -1,7 +1,24 @@
 import {Injectable} from '@angular/core';
+import {User} from '../../users/shared/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SortUsersService {
+  sortByFirstNameAsc(users: User[]): User[] {
+    return users.sort(
+      (
+        category1: User,
+        category2: User
+      ) => {
+        if (category1.firstName > category2.firstName) {
+          return 1;
+        } else if (category1.firstName < category2.firstName) {
+          return -1;
+        }
+
+        return 0;
+      }
+    );
+  }
 }
