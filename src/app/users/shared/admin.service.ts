f1a9bc397f7380a8f1b2bafa33940e8b62ec0fa1
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-01 20:23:26 +0200

    Add method for getting numbers of admins for service

--- src/app/users/shared/admin.service.ts
+++ src/app/users/shared/admin.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AdminNumbersDto} from './admin-numbers.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-  constructor() {
+  private apiUrl: string = 'http://localhost:8080/admin-numbers/';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getAdminNumbers(): Observable<AdminNumbersDto[]> {
+    return this.http.get<AdminNumbersDto[]>(this.apiUrl);
   }
 }
