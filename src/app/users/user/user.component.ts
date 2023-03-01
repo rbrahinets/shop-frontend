668b9431c0560cc6c5dfc35287fc76092db80f65
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-01 23:47:43 +0200

    Add setting user

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from '../shared/user.model';
+import {UserService} from '../shared/user.service';
+import {NavigationService} from '../../shared/navigation.service';
 
 @Component({
   selector: 'shop-user',
@@ -6,9 +9,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  constructor() {
+  user = new User();
+
+  constructor(
+    private userService: UserService,
+    private navigation: NavigationService
+  ) {
   }
 
   ngOnInit(): void {
+    this.setUser();
+  }
+
+  private setUser(): void {
+    this.userService.getUser(
+      this.navigation.getCurrentPathId()
+    ).then(
+      (user: User) => this.user = user
+    );
   }
 }
