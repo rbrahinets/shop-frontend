fa7c635a1160171197e5389a862cd241c97c8584
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-01 20:34:37 +0200

    Add method for setting numbers of admins

--- src/app/admin-panel/admin-number-add/admin-number-add.component.ts
+++ src/app/admin-panel/admin-number-add/admin-number-add.component.ts
@@ -1,14 +1,25 @@
 import {Component, OnInit} from '@angular/core';
-
+import {AdminService} from '../../users/shared/admin.service';
+import {AdminNumberDto} from '../../users/shared/admin-number.dto';
 @Component({
   selector: 'shop-admin-number-add',
   templateUrl: './admin-number-add.component.html',
   styleUrls: ['./admin-number-add.component.css']
 })
 export class AdminNumberAddComponent implements OnInit {
-  constructor() {
+  private adminNumbers: AdminNumberDto[];
+
+  constructor(
+    private adminService: AdminService,
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  private setAdminNumbers(): void {
+    this.adminService.getAdminsNumbers().subscribe(
+      (adminNumbers: AdminNumberDto[]) => this.adminNumbers = adminNumbers
+    );
   }
 }
