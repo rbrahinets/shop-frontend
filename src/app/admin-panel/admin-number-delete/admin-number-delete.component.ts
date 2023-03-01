60d4c3f18f3c295fcd73714d96e2d893859e130b
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-01 21:00:44 +0200

    Add method for validating number of admin

--- src/app/admin-panel/admin-number-delete/admin-number-delete.component.ts
+++ src/app/admin-panel/admin-number-delete/admin-number-delete.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AdminService} from '../../users/shared/admin.service';
 import {AdminNumberDto} from '../../users/shared/admin-number.dto';
+import {AdminNumberValidator} from '../../shared/validators/admin-number.validator';
 
 @Component({
   selector: 'shop-admin-number-delete',
@@ -8,10 +9,12 @@ import {AdminNumberDto} from '../../users/shared/admin-number.dto';
   styleUrls: ['./admin-number-delete.component.css']
 })
 export class AdminNumberDeleteComponent implements OnInit {
+  adminNumber: string;
   private adminsNumbers: AdminNumberDto[];
 
   constructor(
     private adminService: AdminService,
+    private validator: AdminNumberValidator
   ) {
   }
 
@@ -22,6 +25,14 @@ export class AdminNumberDeleteComponent implements OnInit {
   private setAdminNumbers(): void {
     this.adminService.getAdminsNumbers().subscribe(
       (adminNumbers: AdminNumberDto[]) => this.adminsNumbers = adminNumbers
+    );
+  }
+
+  private isValidAdminNumber(): boolean {
+    return this.validator.validate(
+      this.adminNumber,
+      this.adminsNumbers,
+      true
     );
   }
 }
