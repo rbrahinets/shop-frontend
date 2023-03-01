1fc5c730629970418527c8fafbecc2be6982478f
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-02 00:04:05 +0200

    Create new component 'sort-users'

---
+++ src/app/sort/sort-users/sort-users.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+@Component({
+  selector: 'shop-users-sort',
+  templateUrl: './sort-users.component.html',
+  styleUrls: ['./sort-users.component.css']
+})
+export class SortUsersComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
--- src/app/sort/sort.module.ts
+++ src/app/sort/sort.module.ts
@@ -2,18 +2,21 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SortCategoriesComponent} from './sort-categories/sort-categories.component';
 import {SortProductsComponent} from './sort-products/sort-products.component';
+import {SortUsersComponent} from './sort-users/sort-users.component';
 import {SortProductsService} from './shared/sort-products.service';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     SortCategoriesComponent,
-    SortProductsComponent
+    SortProductsComponent,
+    SortUsersComponent
   ],
   providers: [SortProductsService],
   exports: [
     SortProductsComponent,
-    SortCategoriesComponent
+    SortCategoriesComponent,
+    SortUsersComponent
   ]
 })
 export class SortModule {
