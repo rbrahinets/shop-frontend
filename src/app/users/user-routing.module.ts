8b7d67e92084eff815b8a52b7d146b7e25892007
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-01 23:53:24 +0200

    Add routing for page 'user'

--- src/app/users/user-routing.module.ts
+++ src/app/users/user-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {UserComponent} from './user/user.component';
 import {UserDeleteComponent} from './user-delete/user-delete.component';
 import {RoleSuperadminGuard} from '../shared/guards/role-superadmin.guard';
 
 const usersRoutes: Routes = [
   {path: 'users/delete-user', component: UserDeleteComponent, canActivate: [RoleSuperadminGuard]},
+  {path: 'users/:id', component: UserComponent, canActivate: [RoleSuperadminGuard]},
 ];
 
 @NgModule({
