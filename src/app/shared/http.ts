b889f9410880474e896ac8b4a22f9056f9fd517b
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-22 15:34:08 +0200

    Add csrf token to headers

--- src/app/shared/http.ts
+++ src/app/shared/http.ts
@@ -2,9 +2,12 @@ import {HttpHeaders} from '@angular/common/http';
 
 export class Http {
   static getHttpOptions(): object {
+    const csrfToken = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+
     return {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'X-XSRF-TOKEN': csrfToken
       })
     };
   }
