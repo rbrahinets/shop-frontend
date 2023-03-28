61298f899e6870b891e1b5c3dd434ea7c61ff17c
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-28 20:53:54 +0300

    Add required properties

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'shop-image',
@@ -6,6 +6,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./image.component.css']
 })
 export class ImageComponent implements OnInit {
+  @Input() imageBytes: any;
+  @Input() alt: string;
+
   constructor() {
   }
 
