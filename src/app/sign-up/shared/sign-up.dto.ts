4ab475b9c09e1d4623662de2efb516fccad30f50
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-28 16:43:34 +0300

    Update dto

--- src/app/sign-up/shared/sign-up.dto.ts
+++ src/app/sign-up/shared/sign-up.dto.ts
@@ -1,40 +1,9 @@
 export class SignUpDto {
-  constructor(
-    private _firstName: string,
-    private _lastName: string,
-    private _email: string,
-    private _phone: string,
-    private _password: string,
-    private _confirmPassword: string,
-    private _adminNumber: string
-  ) {
-  }
-
-  get firstName(): string {
-    return this._firstName;
-  }
-
-  get lastName(): string {
-    return this._lastName;
-  }
-
-  get email(): string {
-    return this._email;
-  }
-
-  get phone(): string {
-    return this._phone;
-  }
-
-  get password(): string {
-    return this._password;
-  }
-
-  get confirmPassword(): string {
-    return this._confirmPassword;
-  }
-
-  get adminNumber(): string {
-    return this._adminNumber;
-  }
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
+  confirmPassword: string;
+  adminNumber: string;
 }
