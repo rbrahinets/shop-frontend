703c1555c86cf20ad6c3f636fba64998aca544c7
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-04-10 17:50:01 +0300

    Update response headers

--- src/app/shared/http.ts
+++ src/app/shared/http.ts
@@ -2,12 +2,9 @@ import {HttpHeaders} from '@angular/common/http';
 
 export class Http {
   static getHttpOptions(): object {
-    const csrfToken = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, '$1');
-
     return {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'X-XSRF-TOKEN': csrfToken
+        'Content-Type': 'application/json'
       })
     };
   }
