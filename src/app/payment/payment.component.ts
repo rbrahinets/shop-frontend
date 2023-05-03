903ecf5c20e8cbd1d898d45b158c2750a9e76df3
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 11:38:54 +0300

    Add reset cart

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {PaymentService} from './shared/payment.service';
 import {CartService} from '../cart/shared/cart.service';
+import {ProductsCartService} from '../cart/shared/products-cart.service';
 import {PaymentRequest} from './shared/payment-request.dto';
 import {PaymentResponse} from './shared/payment-response.dto';
 import {PaymentValidator} from './shared/payment.validator';
+import {Cart} from '../cart/shared/cart.model';
+import {NavigationService} from '../shared/navigation.service';
 
 @Component({
   selector: 'shop-payment',
@@ -20,6 +23,8 @@ export class PaymentComponent implements OnInit {
   constructor(
     private paymentService: PaymentService,
     private cartService: CartService,
+    private productsCartsService: ProductsCartService,
+    private navigation: NavigationService,
     private validator: PaymentValidator,
   ) {
   }
@@ -53,6 +58,20 @@ export class PaymentComponent implements OnInit {
       (response: PaymentResponse) => {
         alert(response.responseMessage);
         console.log(response.responseMessage);
+        this.resetCart();
+      }
+    );
+  }
+
+  private resetCart(): void {
+    this.cartService.findAll().subscribe(
+      (carts: Cart[]) => {
+        const cart: Cart = CartService.getCartForCurrentUser(carts);
+        cart.totalCost = 0;
+        this.productsCartsService.deleteProductsFromCurrentCart();
+        this.cartService.update(cart).subscribe(
+          () => this.navigation.goToEndpoint('/cart', true)
+        );
       }
     );
   }
