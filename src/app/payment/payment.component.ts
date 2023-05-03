479f716a4c5e609a70d8ba4df6e6d3a90e4ca97a
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 11:37:25 +0300

    Add validation of payment data

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {PaymentRequest} from './shared/payment-request.dto';
+import {PaymentValidator} from './shared/payment.validator';
 
 @Component({
   selector: 'shop-payment',
@@ -6,9 +8,36 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
-  constructor() {
+  @Input() amount: number;
+  cardNumber: string;
+  cardExpiry: string;
+  cardCvc: string;
+  private paymentRequest: PaymentRequest;
+
+  constructor(
+    private validator: PaymentValidator,
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  onPay(): void {
+    this.paymentRequest = new PaymentRequest(
+      this.amount,
+      this.cardNumber,
+      this.cardExpiry,
+      this.cardCvc
+    );
+
+    if (!this.isValidData()) {
+      return;
+    }
+  }
+
+  private isValidData(): boolean {
+    return this.validator.validate(
+      this.paymentRequest
+    );
   }
 }
