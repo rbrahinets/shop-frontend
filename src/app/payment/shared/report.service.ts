8d5513f38e4523ae2563c01e32b8a3276d2b9acf
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 20:27:41 +0300

    Add method for downloading report of payment to service

--- src/app/payment/shared/report.service.ts
+++ src/app/payment/shared/report.service.ts
@@ -1,7 +1,24 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ReportDto} from './report.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
+  private apiUrl = 'http://localhost:8080/api/report';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  downloadReport(reportDto: ReportDto): Observable<any> {
+    return this.http.post(
+      `${this.apiUrl}/download`,
+      reportDto,
+      {responseType: 'blob'}
+    );
+  }
 }
