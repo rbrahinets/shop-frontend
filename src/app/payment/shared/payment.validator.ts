aec25f4ec0bf9462bdac86190b0054394ca39267
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 11:33:34 +0300

    Add method for validation of amount

--- src/app/payment/shared/payment.validator.ts
+++ src/app/payment/shared/payment.validator.ts
@@ -6,4 +6,13 @@ import {Injectable} from '@angular/core';
 export class PaymentValidator {
   constructor() {
   }
+
+  private static validateAmount(amount: number): boolean {
+    if (!amount || amount < 0) {
+      alert('An amount is invalid');
+      return false;
+    }
+
+    return true;
+  }
 }
