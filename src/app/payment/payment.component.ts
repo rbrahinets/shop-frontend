e0a0b550da2fe20ef64fcd7196e4dfb3525e8420
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 11:38:12 +0300

    Add payment

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {PaymentService} from './shared/payment.service';
+import {CartService} from '../cart/shared/cart.service';
 import {PaymentRequest} from './shared/payment-request.dto';
+import {PaymentResponse} from './shared/payment-response.dto';
 import {PaymentValidator} from './shared/payment.validator';
 
 @Component({
@@ -15,6 +18,8 @@ export class PaymentComponent implements OnInit {
   private paymentRequest: PaymentRequest;
 
   constructor(
+    private paymentService: PaymentService,
+    private cartService: CartService,
     private validator: PaymentValidator,
   ) {
   }
@@ -33,11 +38,22 @@ export class PaymentComponent implements OnInit {
     if (!this.isValidData()) {
       return;
     }
+
+    this.payment();
   }
 
   private isValidData(): boolean {
     return this.validator.validate(
       this.paymentRequest
+    );
+  }
+
+  private payment(): void {
+    this.paymentService.payment(this.paymentRequest).subscribe(
+      (response: PaymentResponse) => {
+        alert(response.responseMessage);
+        console.log(response.responseMessage);
+      }
     );
   }
 }
