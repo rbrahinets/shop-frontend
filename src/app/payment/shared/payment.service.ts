7b6ecb15c8042069f336ea76ed766c4989cefc50
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 11:28:31 +0300

    Add method for payment to service

--- src/app/payment/shared/payment.service.ts
+++ src/app/payment/shared/payment.service.ts
@@ -1,9 +1,26 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {PaymentRequest} from './payment-request.dto';
+import {PaymentResponse} from './payment-response.dto';
+import {Http} from '../../shared/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
-  constructor() {
+  private apiUrl = 'http://localhost:8080/api/payment';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  payment(paymentData: PaymentRequest): Observable<PaymentResponse> {
+    return this.http.post<PaymentResponse>(
+      this.apiUrl,
+      paymentData,
+      Http.getHttpOptions()
+    );
   }
 }
