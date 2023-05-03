9f009f5a0b5ab533d74a87a0539ac6c7393ab043
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 20:23:25 +0300

    Add new property to PaymentResponseDto

--- src/app/payment/shared/payment-response.dto.ts
+++ src/app/payment/shared/payment-response.dto.ts
@@ -1,9 +1,12 @@
 export class PaymentResponseDto {
   message: string;
+  isSuccessfully: boolean;
 
   constructor(
     message: string,
+    isSuccessfully: boolean
   ) {
     this.message = message;
+    this.isSuccessfully = isSuccessfully;
   }
 }
