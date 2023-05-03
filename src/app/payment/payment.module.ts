568342da4427ab7f70084ed83a01b36f2624366a
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 11:23:53 +0300

    Create new service for payment

--- src/app/payment/payment.module.ts
+++ src/app/payment/payment.module.ts
@@ -1,13 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 import {PaymentComponent} from './payment.component';
+import {PaymentService} from './shared/payment.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule
   ],
   declarations: [PaymentComponent],
-  providers: [],
+  providers: [PaymentService],
   exports: [PaymentComponent]
 })
 export class PaymentModule {
