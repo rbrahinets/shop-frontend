5307e87aac742acec4ccde9a5500d7a727933653
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 11:42:41 +0300

    Add event for click on pay

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -14,6 +14,7 @@ import {ProductsCartService} from './shared/products-cart.service';
 export class CartComponent implements OnInit {
   cart: Cart;
   groupsOfProducts: ProductsGroupsDto[];
+  showPayment: boolean;
   faTimes = faTimes;
   private productsInCart: Product[];
 
@@ -27,7 +28,12 @@ export class CartComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.showPayment = false;
     this.setCart();
+  }
+
+  onPay(): void {
+    this.showPayment = !this.showPayment;
   }
 
   deleteProductFromCart(productName: string): void {
