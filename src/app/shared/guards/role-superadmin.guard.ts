8873c07bd5dd8ea61ce340e7e2bc05b855afa6d4
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-09 19:57:27 +0300

    Update guard for role superadmin

--- src/app/shared/guards/role-superadmin.guard.ts
+++ src/app/shared/guards/role-superadmin.guard.ts
@@ -1,37 +1,30 @@
 import {inject} from '@angular/core';
-import {UserService} from '../../users/shared/user.service';
 import {LoggedUserService} from '../../users/shared/logged-user.service';
+import {UserService} from '../../users/shared/user.service';
 import {NavigationService} from '../navigation.service';
 import {User} from '../../users/shared/user.model';
 
-let isSuperadmin: boolean;
-
 export const roleSuperadminGuard = (): boolean => {
+  const userService: UserService = inject(UserService);
   const navigation: NavigationService = inject(NavigationService);
-  setIsSuperadmin();
 
   const authorized: boolean = LoggedUserService.getUserId() > 0;
 
   if (!authorized) {
     navigation.goToEndpoint('/sign-in');
     return false;
-  } else if (!isSuperadmin) {
-    navigation.goToEndpoint('/page-not-found');
-    return false;
-  }
-
-  return true;
-};
-
-const setIsSuperadmin = (): void => {
-  const userService: UserService = inject(UserService);
-  if (!LoggedUserService.getUserId()) {
-    return;
   }
 
   userService.findById(
     LoggedUserService.getUserId()
-  ).then(
-    (user: User): boolean => isSuperadmin = user.adminNumber === '13012002'
+  ).subscribe(
+    (user: User): boolean => {
+      if (!(user.adminNumber === '13012002')) {
+        navigation.goToEndpoint('/page-not-found');
+        return false;
+      }
+    }
   );
+
+  return true;
 };
