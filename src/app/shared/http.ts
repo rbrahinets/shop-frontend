4037e92a4f9d955c100623cc700febc1a3b78ce7
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-21 17:47:49 +0300

    Update headers for post requests

--- src/app/shared/http.ts
+++ src/app/shared/http.ts
@@ -1,10 +1,12 @@
 import {HttpHeaders} from '@angular/common/http';
+import {CookieService} from 'ng2-cookies';
 
 export class Http {
-  static getHttpOptions(): object {
+  static getHttpOptions(cookieService: CookieService): object {
     return {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'X-XSRF-TOKEN': cookieService.get('XSRF-TOKEN')
       })
     };
   }
