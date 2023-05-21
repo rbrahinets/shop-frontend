eb4a50ba2b0e9144a6fa7de9b2f8990f503ba6b5
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-21 17:48:37 +0300

    Add service for cookie to 'shared'

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
+import {CookieService} from 'ng2-cookies';
 import {LoggedUserService} from '../users/shared/logged-user.service';
 import {NavigationService} from './navigation.service';
 
 @NgModule({
   providers: [
+    CookieService,
     LoggedUserService,
     NavigationService
   ],
