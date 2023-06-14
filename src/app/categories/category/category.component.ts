a89370fb06de03796ff8fc37ab07e4c7600997a0
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-14 15:48:36 +0300

    Add titles for some pages

--- src/app/categories/category/category.component.ts
+++ src/app/categories/category/category.component.ts
@@ -14,6 +14,7 @@ import {LoggedUserService} from '../../users/shared/logged-user.service';
 })
 export class CategoryComponent implements OnInit {
   userRole: string;
+  categoryName: string;
   products: Product[] = [];
 
   constructor(
@@ -26,6 +27,12 @@ export class CategoryComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.categoryService.findById(
+      this.navigation.getCurrentPathId()
+    )
+      .subscribe(
+        (category) => this.categoryName = category.name
+      );
     this.setProducts();
   }
 
